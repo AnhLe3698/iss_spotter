08ead42d914bb0b15e0693d17059350a4c27cc9d
AnhLe3698 <anhviet.le00@hotmail.com>
2022-07-26 05:23:35 -0600

    Async callback waterfall

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
- 
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
-
-  console.log('It worked! Returned IP:' , ip);
-  fetchCoordsByIP(ip, (error, location) => {
-    if (error) {
-      console.log("We could not fetch the coordinates!" , error);
-      return;
-    }
-    console.log(`It worked! Latitude:${location[0]} Longitude${location[1]}`);
-  });
+  // success, print out the deets!
+  console.log(passTimes);
 });
--- iss.js
+++ iss.js
@@ -60,4 +60,89 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP};
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+  // https://iss-pass.herokuapp.com/json/?lat=YOUR_LAT_INPUT_HERE&lon=YOUR_LON_INPUT_HERE
+
+  request(`https://iss-pass.herokuapp.com/json/?lat=${coords[0]}&lon=${coords[1]}`, (error, response, body) => {
+    let flyByTimes = [];
+    let parsedBody = JSON.parse(body);
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode}. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    if (parsedBody.message === "success") {
+      flyByTimes = parsedBody.response;
+    } else {
+      callback(Error("Very Invalid coordinates"), null);
+      return;
+    }
+      
+    callback(error, flyByTimes);
+  });
+};
+
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results.
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      console.log("It didn't work!" , error);
+      return;
+    }
+  
+    console.log('It worked! Returned IP:' , ip);
+    fetchCoordsByIP(ip, (error, location) => {
+      if (error) {
+        console.log("We could not fetch the coordinates!" , error);
+        return;
+      }
+      console.log(`It worked! Latitude: ${location[0]} Longitude: ${location[1]}`);
+      fetchISSFlyOverTimes(location, (error, flyByTimes) => {
+        if (error) {
+          console.log("Could not fetch the fly over times!", error);
+          return;
+        }
+        console.log(flyByTimes);
+        
+        // Need to convert the flybytimes from epoch times to normal dates
+        // Next pass at Fri Jun 01 2021 13:01:35 GMT-0700 (Pacific Daylight Time) for 465 seconds!
+        let utcSeconds;
+        let d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+        
+        for (const dates of flyByTimes) {
+          utcSeconds = dates.risetime;
+          //utcSeconds = 1658946723;
+          d.setUTCSeconds(utcSeconds);
+          callback(error,`Next pass at ${d} for ${dates.duration}`);
+        }
+      });
+    });
+  });
+};
+
+module.exports = { nextISSTimesForMyLocation};
